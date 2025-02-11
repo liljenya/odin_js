a570933af4a3b8efc050a72c0e46190783f22886
liljenya <kristapen13@gmail.com>
2025-02-11 15:45:29 +0200

    UI IS COMPLETED

--- script.js
+++ script.js
@@ -1,56 +1,63 @@
 function getComputerChoice() {
-    let choice = Math.random(3);
+    let choice = Math.random() * 3;
 
-    if (choice === 0) {
+    if (choice < 1) {
         return 'r';
-    }
-    else if (choice === 1) {
+    } else if (choice < 2) {
         return 'p';
-    }
-    else {
+    } else {
         return 's';
     }
 }
 
-function getHumanChoice() {
-    return window.prompt("What do you want to choose(R/P/S)?").toLowerCase();
-}
-
-function playRound(humanScore, computerScore) {
-    const humanSelection = getHumanChoice();
+function playRound(humanSelection) {
     const computerSelection = getComputerChoice();
     if (humanSelection === computerSelection) {
-        alert('Draw');
-    }
-    else if (humanSelection === 'r' && computerSelection === 'p' || humanSelection === 'p' && computerSelection === 's') {
-        alert('Computer won');
-        computerScore++;
-    }
-    else if (humanSelection === 'r' && computerSelection === 's' || humanSelection === 's' && computerSelection === 's') {
-        alert('User won');
-        humanScore++;
-    }
-    console.log('Computer\'s score:' + computerScore)
-    console.log('User\'s score:' + humanScore)
-    console.log(`U : ${humanScore} \| C : ${computerScore}`);
-}
-
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        playRound(humanScore, computerScore);
-    }
-
-    if(humanScore > computerScore){
-        alert('Human has won hahaha molodec')
-    }
-    else if(humanScore < computerScore){
-        alert('Computer has won hahaha molodec')
-    }
-    else{
-        alert('Nobody has won grusni')
+        document.getElementById("results").textContent = 'Draw';
+    } else if (humanSelection === 'r' && computerSelection === 'p' || humanSelection === 'p' && computerSelection === 's' || humanSelection === 's' && computerSelection === 'r') {
+        document.getElementById("results").textContent = 'Computer won';
+        document.getElementById("computer-score").textContent = ++computerScore;
+    } else {
+        document.getElementById("results").textContent = 'Human won';
+        document.getElementById("player-score").textContent = ++humanScore;
     }
 }
 
-playGame();
+function play(humanSelection) {
+    if (round++ < 5) {
+        playRound(humanSelection);
+        if (round === 5) {
+            let modal = document.getElementById("modal");
+            let modalText = document.getElementById("modal-text");
+            if (humanScore > computerScore) {
+                modalText.textContent = 'Human has won hahaha molodec';
+            } else if (humanScore < computerScore) {
+                modalText.textContent = 'Computer has won hahaha molodec';
+            } else {
+                modalText.textContent = 'Nobody has won grusni';
+            }
+            modal.style.display = 'block';
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('closeModal').addEventListener('click', function () {
+        document.getElementById('modal').style.display = 'none';
+    });
+});
+
+let round = 0;
+let humanScore = 0;
+let computerScore = 0;
+
+window.addEventListener("DOMContentLoaded", () => {
+    const rockBtn = document.getElementById("rock");
+    rockBtn.addEventListener("click", () => play("r"));
+
+    const paperBtn = document.getElementById("paper");
+    paperBtn.addEventListener("click", () => play("p"));
+
+    const scissorsBtn = document.getElementById("scissors");
+    scissorsBtn.addEventListener("click", () => play("s"));
+});
